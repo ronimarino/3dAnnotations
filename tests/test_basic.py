89f4f67f8ef71490c4ec8c5151380b8a45619f05
Marinovic, Hrvoje AVL HR <hrvoje.marinovic@avl.com>
2021-03-14 21:09:04 +0100

    module rename

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from .context import annotations
+from 3dAnnotations import annotations
+import helpers
 
 import unittest
 
+'''
+To have a complete set of manual tests,
+all you need to do is make a list of all the features your application has,
+the different types of input it can accept, and the expected results.
+'''
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
-    def test_absolute_truth_and_meaning(self):
+    def test_euler_to_quaternion(self):
         assert True
 
 
