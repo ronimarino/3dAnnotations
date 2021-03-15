46b27640cf751f4a46db063229b386d6604b0fa2
Marinovic, Hrvoje AVL HR <hrvoje.marinovic@avl.com>
2021-03-15 11:44:36 +0100

    fixed context.py in tests

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../Annotations3d/Annotations3d'))
+sys.path.insert(0, os.path.abspath('Annotations3d'))
 
 import Annotations3d
 import helpers
